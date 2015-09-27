cb6fc938b6f7fe8e50f4109296098bb741888c43
luiarthur <luiarthur@gmail.com>
2015-09-26 20:21:13 -0700

    yes

--- _includes/code/ams241/01/dp.R
+++ _includes/code/ams241/01/dp.R
@@ -1,10 +1,10 @@
-N <- 100 # number of observations?
-a <- 6 # alpha
+N <- 1000 # number of observations?
+a <- 3 # alpha
 
-#rG <- function(num) rgamma(num,3,3)
+rG <- function(num) rgamma(num,3,scale=3)
 #rG <- function(num) rbeta(num,2,3)
 #rG <- function(num) rnorm(num,0,1)
-rG <- function(num) rpois(num,4)
+#rG <- function(num) rpois(num,4)
 
 x <- rep(0,N)
 x[1] <- rG(1)
@@ -18,7 +18,9 @@ for (i in 2:N) {
 }
 
 # Plot Results
-opar <- par()
-hist(rG(1e6),freq=F,add=F,col="lightgrey",border="white",xlab="x",
-     ylab="Probability",main="")
-lines(table(x) / sum(table(x)) ,col="blue")
+#png("../../../../assets/ams241/01/plots/dp_gamma.png"); pmar <- par("mar")
+#par("mar"=c(4,4,1,0))
+  hist(rG(1e6),freq=F,add=F,col="lightgrey",border="white",xlab="x",
+       ylab="Probability",main="")
+  lines(table(x) / sum(table(x)) ,col="blue")
+#par("mar"=pmar); dev.off()
