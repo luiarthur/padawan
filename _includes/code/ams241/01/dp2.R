616a67b3e5596fa51a18b4a140350e667fe1e3ba
luiarthur <luiarthur@gmail.com>
2015-09-28 13:44:20 -0700

    yes

--- _includes/code/ams241/01/dp2.R
+++ _includes/code/ams241/01/dp2.R
@@ -15,16 +15,52 @@ dp <- function(rG,a,N=100,printProg=F) {
   x
 }
 
-rG <- function(n) rnorm(n)
+rG <- function(n) rnorm(n,0,1)
+pG <- function(x) pnorm(x,0,1)
 
-a <- 3
+a <- 100
 N <- 100
-plot.cdf(dp(rG,a,N),discrete=F,print=F,cex=.5,pch=20,
-         col=rgb(.5,.5,.5,1),ylim=c(0,1),xlim=c(-3,3))
-for (i in 1:1000) {
-  plot.cdf(dp(rG,a,N),discrete=F,print=F,add=T,cex=.5,pch=20,
-           col=rgb(.5,.5,.5,.1))
-  cat("\r Progress:",i,"/",1000)
+B <- 1000
+X <- matrix(0,B,N)
+for (i in 1:B) {
+  X[i,] <- dp(rG,a,N)
+  plot.cdf(X[i,],
+           discrete=F,print=F,add=ifelse(i==1,F,T),
+           cex=.5,pch=20,
+           col=ifelse(i==B,"black",rgb(.5,.5,.5,.2)),
+           ylim=c(0,1),xlim=c(-3,3),
+           ylab="Fn(x)",xlab="x",main="CDF"
+           )
+  cat("\r Progress:",i,"/",B)
 }
 
-curve(pnorm,add=T,lwd=2,col="red")
+curve(pG,add=T,lwd=2,col="red")
+X.sort <- t(apply(X,1,function(r) sort(r)))
+X.mean <- apply(X.sort,2,mean)
+plot.cdf(X.mean,discrete=F,print=F,add=T,col="blue",lwd=3)
+
+#Linear Interpolation#################
+tY <- apply(X.sort,1,function(x) {
+               #ux <- sort(unique(x))
+               ux <- x
+               lux <- length(ux)
+               cdf <- double(lux)
+
+               for (i in 1:lux) {
+                 if (i>1) cdf[i-1] <- sum(ux[i] > x)
+               }
+
+               cdf <- cdf / length(x)
+               cdf[lux] <- 1
+               cdf
+           })
+Y <- t(tY)
+xout = seq(-4, 4, length = 100)
+
+Z <- matrix(0, B, N)
+for (i in 1:B)
+  Z[i,] <- approx(X.sort[i,], Y[i,], xout = xout)$y
+  #Z[i,] <- approx(unique(X.sort[i,]), Y[[i]], xout = xout)$y
+Zmeans = apply(Z, 2, mean, na.rm = TRUE)
+lines(xout, Zmeans, col = 'green',lwd=2)
+abline(h=.5,lwd=1.5)
