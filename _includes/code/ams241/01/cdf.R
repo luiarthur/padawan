4c6e57967b7be474b581ec04d56ca575975be13a
luiarthur <luiarthur@gmail.com>
2015-09-27 22:22:55 -0700

    yes

--- _includes/code/ams241/01/cdf.R
+++ _includes/code/ams241/01/cdf.R
@@ -1,17 +1,22 @@
 set.seed(1)
-x <- rpois(10,2)
-ux <- sort(unique(x))
-lux <- length(ux)
-cdf <- matrix(0,lux,2)
 
-for (i in 1:lux) {
-  cdf[i,1] <- ux[i]
-  if (i>1) cdf[i-1,2] <- sum(ux[i] > x)
+plot.cdf <- function(x,discrete=T,...) {
+  ux <- sort(unique(x))
+  lux <- length(ux)
+  cdf <- matrix(0,lux,2)
+
+  for (i in 1:lux) {
+    cdf[i,1] <- ux[i]
+    if (i>1) cdf[i-1,2] <- sum(ux[i] > x)
+    cat("\r Progress: ",i,"/",lux)
+  }
+  cat("\n")
+
+  cdf[,2] <- cdf[,2] / length(x)
+  cdf[lux,2] <- 1
+
+  plot(cdf[,1],cdf[,2],type=ifelse(discrete,"p","l"),...)
 }
 
-cdf[,2] <- cdf[,2] / length(x)
-cdf[lux,2] <- 1
-cdf
-
-plot(ecdf(x))
-plot(cdf[,1],cdf[,2])
+y <- rnorm(1e3,20)
+plot.cdf(y,pch=20,discrete=T)
