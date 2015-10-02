e0fb6c160a5b275d9e3ded62bab93107967b7024
Arthur Lui <luiarthur@gmail.com>
2015-10-02 15:49:54 -0700

    yes

--- _includes/code/ams241/03/dp_posteior.R
+++ _includes/code/ams241/03/dp_posteior.R
@@ -16,21 +16,21 @@ dp <- function(N=1,pG,a,xlim=c(0,1),n=1000) {
 }
 
 ########
-#N <- 100
-#a <- 100
-#xlim <- c(-3,3)
-#n <- 9
-#pG <- function(x) pnorm(x)
-#
-#X <- dp(N=N,pG=pG,a=a,xlim=xlim,n=n)
-#plot(0,xlim=range(X$x),ylim=c(0,1),cex=0)
-#for (i in 1:nrow(X$G)) {
-#  lines(X$x,X$G[i,],type="l",col=rgb(.4,.4,.4),lwd=.1)
-#  cat("\r", i/nrow(X$G))
-#}
-#curve(pnorm,add=T,col="darkred",lwd=3)
-#EG <- apply(X$G,2,mean)
-#lines(X$x,EG,col=rgb(0,0,1,.4),lwd=5)
+N <- 100
+a <- 100
+xlim <- c(-3,3)
+n <- 9
+pG <- function(x) pnorm(x)
+
+X <- dp(N=N,pG=pG,a=a,xlim=xlim,n=n)
+plot(0,xlim=range(X$x),ylim=c(0,1),cex=0)
+for (i in 1:nrow(X$G)) {
+  lines(X$x,X$G[i,],type="l",col=rgb(.4,.4,.4),lwd=.1)
+  cat("\r", i/nrow(X$G))
+}
+curve(pnorm,add=T,col="red",lwd=3)
+EG <- apply(X$G,2,mean)
+lines(X$x,EG,col=rgb(0,0,1,.4),lwd=5)
 
 y <- c(rnorm(100,7,2),rgamma(100,2,3))
 plot(density(y))
