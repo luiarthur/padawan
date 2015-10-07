6a1cb7a02505e94530dc0f96539931d8013c4af7
luiarthur <luiarthur@gmail.com>
2015-10-07 13:03:10 -0700

    yes

--- _includes/code/ams241/project_ideas/lda/lda.R
+++ _includes/code/ams241/project_ideas/lda/lda.R
@@ -14,19 +14,34 @@ V <- sum(N)
 K <- 5     # Number of topics preset to 5
 
 # GIBBS
+rdir <- function(N,a) {
+  k <- length(a)
+  x <- matrix(rgamma(k*N, a, 1), N, k, byrow=T)
+  rowsums <- x %*% rep(1,k)
+  x / as.vector(rowsums)
+}
+
 B <- 1000
 
 # Preallocate
-a <- matrix(.1,B,K)
-b <- matrix(.001,B,K)
-psi <- matrix(1/V,B,V)
-theta <- matrix(1/V,B,V)
+psi <- as.list(1:B); psi[[1]] <- matrix(1/V,K,V)
+theta <- as.list(1:B); theta[[1]] <- matrix(1/K,M,K)
+z <- x
+for (d in 1:M) z[[d]] <- sample(1:K,N[d],replace=T)
+al <- matrix(.1,B,K)
+be <- matrix(.001,B,V)
 
-for (b in 1:B) {
+for (b in 2:B) {
   # Update:
   for (d in 1:M) {
     for (w in 1:N[d]) {
-
-    }
-  }
-}
+      for (k in 1:K) {
+        xx <- x[which(z == k)]
+        # be[b,] <- be[b-1,] + sum(x == ) # Stuck here
+        psi[[b]][k,] <- rdir(1, be[b,])
+      } # k in 1:K
+    } # w in 1:N[d]
+    al[b,] <- al[b-1,] + sum(z == k)
+    theta[[b]][d,] <- rdir(1, be[b,])
+  } # d in 1:M
+} # b in 1:B
