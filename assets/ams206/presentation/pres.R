3ea38b5bae8dc413bdef428ebc3033db4dd3e2cd
luiarthur <luiarthur@gmail.com>
2016-01-23 21:55:08 -0800

    yes

--- assets/ams206/presentation/pres.R
+++ assets/ams206/presentation/pres.R
@@ -1,12 +1,17 @@
 #7
-a <- seq(-1,1,len=10000)
+a <- seq(-3,3,len=10000)
 f <- function(x) exp(x) - x - 1
 
-par(mar=c(4.2,5,4,1))
+pdf("img/fig1.pdf")
+par(mar=c(4.2,5,1,1))
 plot(a,f(a),col='grey',lwd=3,type='l',bty='n',
      xlab=expression(theta-theta(x)),
      ylab=expression( "L ="~ e^{c(theta-theta(x))} - c(theta-theta(x)) - 1))
-lines(a,f(a*.1),lwd=4,type='l',bty='n',col='red')
-lines(a,f(a*.5),lwd=4,type='l',bty='n',col='blue')
-lines(a,f(a* 1),lwd=4,type='l',bty='n',col='green')
-lines(a,f(a* 2),lwd=4,type='l',bty='n',col='orange')
+lines(a,f(a*.1),lwd=3,type='l',bty='n',col='red')
+lines(a,f(a*.5),lwd=3,type='l',bty='n',col='blue')
+lines(a,f(a* 1),lwd=3,type='l',bty='n',col='grey')
+lines(a,f(a* 2),lwd=3,type='l',bty='n',col='green')
+legend("topleft",legend=(paste0("c = ",c(2,1,.5,.1))),bty='n',
+       col=c('green','grey','blue','red'),lwd=3,cex=1.5)
+
+dev.off()
