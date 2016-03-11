a9b57309338c3bc61381e6e530aef3d8bb384569
luiarthur <luiarthur@gmail.com>
2016-03-10 22:36:40 -0800

    yes

---
+++ assets/ams205/final/code/1b.R
@@ -0,0 +1,31 @@
+source("1.R")
+## Computation: ######################
+#mle <- 1.97, -1.50, 1.75, 3.10, .0198
+
+Y <- orb$Y
+X <- orb$X
+a <- mle[1]
+b <- mle[2]
+w <- mle[3]
+d <- mle[4]
+s2 <- mle[5]
+
+h <- function(x,par=mle) {
+  hi <- function(xi) {
+    par[1] + par[2] * cos(par[3]*xi + par[4])
+  }
+
+  out <- NULL
+  if (length(x) == 1) {
+    out <- hi(x)
+  } else {
+    out <- sapply(x,hi)
+  }
+
+  names(out) <- NULL
+  out
+}
+
+l_ww <- sum(b*X^2*(cos(X*w+d)*(Y-h(X)) - b*sin(X*w+d)^2)) / s2
+I_w <- -l_ww
+se_w <- 1/sqrt(I_w)
